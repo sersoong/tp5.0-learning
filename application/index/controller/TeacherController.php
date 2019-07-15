9dc8e78206c81f4d55d7b40909410d1eaa578766
sersoong <sersoong@gmail.com>
2019-07-15 16:23:31 +0800

    插入数据测试

--- application/index/controller/TeacherController.php
+++ application/index/controller/TeacherController.php
@@ -8,13 +8,32 @@ class TeacherController extends Controller
 {
     public function index()
     {
+        //创建Teacher 实例
         $Teacher = new Teacher;
+        //查询所有Teacher数据
         $teachers = $Teacher->select();
-
+        //给模板变量teachers赋值
         $this->assign('teachers',$teachers);
-
+        //加载模板
         $htmls = $this->fetch();
-
+        //返回数据
         return $htmls;
+    }
+
+    public function insert()
+    {
+        //新建测试数据
+        $teacher = array();
+        $teacher['name'] = '王五';
+        $teacher['username'] = 'wangwu';
+        $teacher['sex'] = '1';
+        $teacher['email'] = 'wangwu@qq.com';
+
+        //引用teacher数据表对应的模型
+        $Teacher = new Teacher();
+        
+        //插入数据并判断结果
+        $state = $Teacher->data($teacher)->save();
+        var_dump($state);
     }
 }
