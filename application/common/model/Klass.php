b6ae8fe8d056db600d127a77837a02b07618177f
sersoong <sersoong@gmail.com>
2019-07-19 15:01:06 +0800

    数据列表index

--- application/common/model/Klass.php
+++ application/common/model/Klass.php
@@ -4,7 +4,17 @@ use think\Model;
 
 class Klass extends Model 
 {
-    
+    private $Teacher;
+
+    public function getTeacher()
+    {
+        if (is_null($this->Teacher)) {
+            $teacherId = $this->getData('teacher_id');
+            $this->Teacher = Teacher::get($teacherId);
+        }
+
+        return $this->Teacher;
+    }
 }
 
 
