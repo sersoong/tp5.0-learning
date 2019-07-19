112aac2e7ddad4606525db9a1e2e41b9176fb2c3
sersoong <sersoong@gmail.com>
2019-07-19 15:31:44 +0800

    增加数据add

--- application/index/controller/KlassController.php
+++ application/index/controller/KlassController.php
@@ -1,6 +1,8 @@
 <?php
 namespace app\index\controller;
+use app\common\model\Teacher;
 use app\common\model\Klass;
+use think\Request;
 
 class KlassController extends IndexController
 {
@@ -9,5 +11,31 @@ class KlassController extends IndexController
         $Klasses = Klass::paginate();
         $this->assign('klasses',$Klasses);
         return $this->fetch();
+    }
+
+    public function add()
+    {
+        //获取所有教师信息
+        $teachers = Teacher::all();
+        $this->assign('teachers',$teachers);
+        return $this->fetch();
+    }
+
+    public function save()
+    {
+        //实例化请求信息
+        $Request = Request::instance();
+        
+        //实例化班级并赋值
+        $Klass = new Klass();
+        $Klass->name = $Request->post('name');
+        $Klass->teacher_id = $Request->post('teacher_id/d');
+
+        //添加数据
+        if(!$Klass->validate(true)->save($Klass->getData())){
+            return $this->error('数据添加错误：' . $Klass->getError());
+        }
+
+        return $this->success('操作成功',url('index'));
     }
 }
