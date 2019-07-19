35f374bffbc241fd5403fa80b64fad712ecb078f
sersoong <sersoong@gmail.com>
2019-07-19 16:22:01 +0800

    编辑数据edit

--- application/index/controller/KlassController.php
+++ application/index/controller/KlassController.php
@@ -38,4 +38,47 @@ class KlassController extends IndexController
 
         return $this->success('操作成功',url('index'));
     }
+
+    public function edit()
+    {
+        $id = Request::instance()->param('id/d');
+        
+        //获取所有的教师信息
+        $teachers=Teacher::all();
+        $this->assign('teachers',$teachers);
+
+        //获取用户操作的班级信息
+        if (false === $Klass = Klass::get($id)){
+            return $this->error('系统未找到ID为' . $id . '的记录');
+        }
+
+        $this->assign('Klass',$Klass);
+        return $this->fetch();
+    }
+
+    public function update()
+    {
+        $id = Request::instance()->post('id/d');
+
+        //获取传入的班级信息
+        $Klass = Klass::get($id);
+        if (\is_null($Klass)) {
+            return $this->error('系统未找到ID为' . $id . '的记录');
+        }
+
+        //数据更新
+        $Klass->name=Request::instance()->post('name');
+        $Klass->teacher_id = Request::instance()->post('teacher_id/d');
+        if (!$Klass->validate()->save($Klass->getData())) {
+            return $this->error('更新错误:' . $Klass->getError());
+        } else {
+            return $this->success('操作成功',url('index'));
+        }
+    }
+
+    public function delete()
+    {
+        $id = Request::instance()->param('id/d');
+        var_dump($id);
+    }
 }
