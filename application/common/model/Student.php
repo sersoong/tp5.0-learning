d4a8047a45bdbff95993c1cad506ab11b657a0bf
sersoong <sersoong@gmail.com>
2019-07-22 09:28:04 +0800

    完善显示性别

--- application/common/model/Student.php
+++ application/common/model/Student.php
@@ -4,5 +4,14 @@ use think\Model;
 
 class Student extends Model 
 {
-    
+    public function getSexAttr($value)
+    {
+        $status = array('0'=>'男','1'=>'女');
+        $sex = $status[$value];
+        if(isset($sex)) {
+            return $sex;
+        } else {
+            return $status[0];
+        }
+    }
 }
