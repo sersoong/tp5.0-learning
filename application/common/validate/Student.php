d678b3efb87a553913370ed7350227a73fb7ae32
sersoong <sersoong@gmail.com>
2019-07-22 15:53:10 +0800

    student编辑数据

---
+++ application/common/validate/Student.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\common\validate;
+
+use think\Validate;
+
+class Student extends Validate 
+{
+    protected $rule = [
+        'name'  => 'require|length:2,25',
+        'sex' => 'in:0,1',
+        'klass_id'=> 'require',
+        'email' => 'email',
+    ];
+}
--- application/index/controller/StudentController.php
+++ application/index/controller/StudentController.php
@@ -1,6 +1,8 @@
 <?php
 namespace app\index\controller;
 use app\common\model\Student;
+use app\common\model\Klass;
+use think\Request;
 
 class StudentController extends IndexController 
 {
@@ -12,5 +14,44 @@ class StudentController extends IndexController
         //显示数据
         $this->assign('students',$students);
         return $this->fetch();
+    }
+
+    public function edit()
+    {
+        //获取传入id
+        $id = Request::instance()->param('id/d');
+
+        //判断是否存在该id的记录
+        if(is_null($Student=Student::get($id))) {
+            return $this->error('未找到ID为' . $id . '的记录',url('index'));
+        }
+
+        //取出studetn对象
+        $this->assign('Student',$Student);
+        return $this->fetch();
+    }
+
+    public function update()
+    {
+        //获取传入的数据
+        $id = Request::instance()->post('id/d');
+
+        //判断是否存在该id的记录
+        if(is_null($Student=Student::get($id))) {
+            return $this->error('未找到ID为' . $id . '的记录',url('index'));
+        }
+
+        //修改student对象
+        $Student->name = Request::instance()->post('name');
+        $Student->num = Request::instance()->post('num');
+        $Student->sex = Request::instance()->post('sex');
+        $Student->klass_id = Request::instance()->post('klass_id');
+        $Student->email = Request::instance()->post('email');
+        //写入student对象
+        if (!$Student->validate(true)->save($Student->getData())) {
+            return $this->error('更新错误:' . $Student->getError());
+        } else {
+            return $this->success('操作成功',url('index'));
+        }
     }
 }
