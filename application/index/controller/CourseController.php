3e56dada8afa74689a133abc8f6464c6bc6cf87e
sersoong <sersoong@gmail.com>
2019-07-23 22:03:47 +0800

    course index

--- application/index/controller/CourseController.php
+++ application/index/controller/CourseController.php
@@ -9,6 +9,10 @@ class CourseController extends IndexController
     public function index()
     {
         //取出数据库课程
+        $Course = Course::paginate();
+
+        //赋予变量到模板
+        $this->assign('courses',$Course);
         return $this->fetch();
     }
 
@@ -22,6 +26,16 @@ class CourseController extends IndexController
 
     public function save()
     {
+        $Course = new Course();
+        $Course->name = Request::instance()->post('name');
+
+        // 新增数据并验证。验证类，自己写下吧。
+        if (!$Course->validate(true)->save()) {
+            return $this->error('保存错误：' . $Course->getError());
+        }
+
+        return $this->success('操作成功', url('index'));
+
         var_dump(Request::instance()->param());
     }
 }
