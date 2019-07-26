d78b98f267b4f05077e14fa03c60cca18cddd674
sersoong <sersoong@gmail.com>
2019-07-26 22:04:29 +0800

    增加cmd脚本

---
+++ cmd/start.sh
@@ -0,0 +1,4 @@
+git pull
+sudo docker start sersoong-httpd-xdebug sersoong-mysql
+code .
+nohup dbeaver >/dev/null 2>&1 &
---
+++ cmd/stop.sh
@@ -0,0 +1,7 @@
+sudo killall dbeaver
+sudo killall code
+sudo docker stop sersoong-httpd-xdebug sersoong-mysql
+git add -A
+git commit -m "a"
+git commit --amend
+git push
