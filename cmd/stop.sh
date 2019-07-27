b2201f7db3039b12eb1624779e8e8c99d3740a8f
sersoong <sersoong@gmail.com>
2019-07-27 09:36:08 +0800

    更新stop.sh

--- cmd/stop.sh
+++ cmd/stop.sh
@@ -1,10 +1,15 @@
-ID=`ps -ef | grep dbeaver | grep -v "grep"`
+ID=`ps -ax | grep dbeaver | grep -v "grep" | grep "java" | awk {'print $1'}`
 for id in $ID
 do
-kill -9 $id
+sudo kill -9 $id
 done
 
-sudo killall code
+ID=`ps -ax | grep visual-studio-code | grep -v "grep" | awk {'print $1'}`
+for id in $ID
+do
+sudo kill -9 $id
+done
+
 sudo docker stop sersoong-httpd-xdebug sersoong-mysql
 git add -A
 git commit -m "a"
